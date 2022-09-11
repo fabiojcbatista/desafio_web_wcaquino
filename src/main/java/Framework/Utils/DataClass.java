f751dd83d661a65ab61474f7c7be97cdfbca22eb
Fabio Jr <fabiojcbweb@gmail.com>
2022-09-11 20:48:23 -0300

    Projeto finalizado e refatorado

--- src/main/java/Framework/Utils/DataClass.java
+++
@@ -1,7 +0,0 @@
-package Framework.Utils;
-
-public class DataClass {
-    public static Object[][] contaTestData() {
-        return new Object[][]{{"Aluguel"}, {"Energia Elétrica"}, {"Internet"}, {"Salário"}, {"Consultoria"}, {"Juros por antecipação"}};
-    }
-}
